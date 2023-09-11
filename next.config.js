4b5f97da3198297bda322199bab241a9ca47565e
Konosuke Sakai <konosuke@konosuke.dev>
2023-09-11 15:36:51 +0900

    chore: add prefix support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const prefix = process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '/'
+
 const nextConfig = {
+  assetPrefix: prefix,
+  basePrefix: prefix,
   output: 'export',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
